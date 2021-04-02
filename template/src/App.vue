db704f3a763e7bc1ad1442422b6e95a65c03ca46
Sergey Elsukov <oct8cat@gmail.com>
2021-04-02 14:21:33 +0300

    Move App to AidboxExample

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,62 +1,16 @@
 <template>
   <div id="app">
-    <div v-if="error" class="error">Error: {{ error }}</div>
-    <div v-if="userInfo">
-      <UserInfo :userInfo="userInfo" @logout="setUserInfo(null)" />
-      <Patients />
-    </div>
-    <div v-else-if="ready">
-      <Login @success="setUserInfo" @error="setError" />
-    </div>
+    <AidboxExample />
   </div>
 </template>
 
 <script>
-import Patients from "./components/Patients";
-import Login from "./components/Login";
-import UserInfo from "./components/UserInfo";
+import AidboxExample from "./components/AidboxExample.vue";
 
 export default {
   name: "App",
-  inject: ["aidbox"],
   components: {
-    Login,
-    UserInfo,
-    Patients,
-  },
-  data() {
-    return {
-      error: null,
-      userInfo: null,
-      ready: false,
-    };
-  },
-  mounted() {
-    this.fetchUserInfo();
-  },
-  methods: {
-    setUserInfo(userInfo) {
-      this.setError(null);
-      this.userInfo = userInfo;
-    },
-    setError(error) {
-      this.error = error;
-    },
-
-    async fetchUserInfo() {
-      const res = await this.aidbox.getUserInfo();
-
-      if (res.status === 200) {
-        this.setUserInfo(res.data);
-      } else if (res.status === 0) {
-        this.setError("Aidbox server is unreachable.");
-      }
-      this.ready = true;
-    },
+    AidboxExample,
   },
 };
 </script>
-
-<style>
-@import "main.css";
-</style>
---
+++ template/src/components/AidboxExample.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div v-if="error" class="error">Error: {{ error }}</div>
+    <div v-if="userInfo">
+      <UserInfo :userInfo="userInfo" @logout="setUserInfo(null)" />
+      <Patients />
+    </div>
+    <div v-else-if="ready">
+      <Login @success="setUserInfo" @error="setError" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Patients from "./Patients";
+import Login from "./Login";
+import UserInfo from "./UserInfo";
+
+export default {
+  name: "App",
+  inject: ["aidbox"],
+  components: {
+    Login,
+    UserInfo,
+    Patients,
+  },
+  data() {
+    return {
+      error: null,
+      userInfo: null,
+      ready: false,
+    };
+  },
+  mounted() {
+    this.fetchUserInfo();
+  },
+  methods: {
+    setUserInfo(userInfo) {
+      this.setError(null);
+      this.userInfo = userInfo;
+    },
+    setError(error) {
+      this.error = error;
+    },
+
+    async fetchUserInfo() {
+      const res = await this.aidbox.getUserInfo();
+
+      if (res.status === 200) {
+        this.setUserInfo(res.data);
+      } else if (res.status === 0) {
+        this.setError("Aidbox server is unreachable.");
+      }
+      this.ready = true;
+    },
+  },
+};
+</script>
+
+<style>
+@import "../main.css";
+</style>
